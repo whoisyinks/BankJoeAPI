b39909e3ea4dc98355b47c54c107025cbe79a950
whoisyinks <Olayinks15@gmail.com>
2024-09-12 20:57:01 +0000

    Add project files.

---
+++ Data/BankJoeAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BankJoeAPI.Model;
+
+namespace BankJoeAPI.Data
+{
+    public class BankJoeAPIContext : DbContext
+    {
+        public BankJoeAPIContext (DbContextOptions<BankJoeAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BankJoeAPI.Model.Country> Country { get; set; } = default!;
+    }
+}
